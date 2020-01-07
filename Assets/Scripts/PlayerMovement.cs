aa5a2970e18b67625cd33d251541870aa591b418
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-01-07 05:51:36 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    
+    private float horizontalMove = 0f;
+    private Rigidbody2D rb;
+
+    private bool jump = false;
+    private bool jumpHeld = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        jumpHeld = Input.GetButton("Jump");
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime);
+
+        if (jump)
+        {
+            controller.Jump();
+            jump = false;
+        }
+
+        // "Better Jump" code
+        if (rb.velocity.y < -0.01)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (rb.velocity.y > 0.01 && !jumpHeld)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+}
