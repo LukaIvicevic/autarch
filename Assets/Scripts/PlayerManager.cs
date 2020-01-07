b7cd222e187c5e4251cca5aaa2abaf2bc8f9f8dd
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-01-07 06:34:17 -0600

    Working on player manager

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class PlayerManager
+{
+    public static int PlayerCount { 
+        get
+        { 
+            return Players.Count(x => x);
+        }
+    }
+
+    public static bool[] Players = new bool[] { false, false, false, false };
+
+    public static void AddPlayer(int playerNumber)
+    {
+        Players[playerNumber] = true;
+    }
+
+    public static void RemovePlayer(int playerNumber)
+    {
+        Players[playerNumber] = false;
+    }
+}
---
+++ Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Add players when they press fire button
+        if (Input.GetButtonDown("Fire1_P1"))
+        {
+            PlayerManager.AddPlayer(1);
+        }
+        if (Input.GetButtonDown("Fire1_P2"))
+        {
+            PlayerManager.AddPlayer(2);
+        }
+        if (Input.GetButtonDown("Fire1_P3"))
+        {
+            PlayerManager.AddPlayer(3);
+        }
+        if (Input.GetButtonDown("Fire1_P4"))
+        {
+            PlayerManager.AddPlayer(4);
+        }
+    }
+}
