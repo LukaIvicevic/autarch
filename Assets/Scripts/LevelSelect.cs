7d883f58093ca7fb6893ce76a0f7a2fcf381c39f
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-01-08 18:35:40 -0600

    Create level select button prefab

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public string LevelName;
+    public TextMeshProUGUI ButtonText;
+
+    private void Awake()
+    {
+        ButtonText.text = LevelName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadLevel()
+    {
+        // TODO: Use LoadManager
+        SceneManager.LoadScene(LevelName);
+    }
+}
