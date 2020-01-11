1cb18724f7252ed02c0690492889c7f794390651
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-01-11 17:50:09 -0600

    Fire a bullet from the pistol

--- Assets/Pistol.cs
+++ Assets/Pistol.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Pistol : MonoBehaviour, IWeapon
 {
+    public Transform firePoint;
+    public GameObject bullet;
+    public float fireDelay = 1f;
+
+    private float canFireTime = 0;
+
     public void Fire()
     {
-        Debug.Log("Pew Pew");
+        if (Time.time >= canFireTime)
+        {
+            Debug.Log("Pew Pew");
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+            canFireTime = Time.time + fireDelay;
+        }
     }
 
     // Start is called before the first frame update
