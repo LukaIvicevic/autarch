fbe6573c30f6c8e405c0bca524c2815d58869a4d
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-01-19 13:59:52 -0600

    Add rocket rotation while in air

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -9,10 +9,23 @@ public class Rocket : MonoBehaviour
     public Rigidbody2D rb;
     public CharacterController2D firedBy;
 
+    private Vector3 originalPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
+        originalPosition = gameObject.transform.position;
+    }
+
+    private void Update()
+    {
+        var moveDirection = gameObject.transform.position - originalPosition;
+        if (moveDirection != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
