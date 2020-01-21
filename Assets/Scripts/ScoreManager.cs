9af5f9fc95aa5430c4f5edafb7f95f17cbd16412
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-01-21 06:30:54 -0600

    Working on end of round screen

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿
 public static class ScoreManager
 {
-    public static int[] Scores = new int[] { 0, 0, 0, 0 };
+    public static int[] Scores;
 
     public static int PointsPerKill = 1;
 
     public static int SuicidePenalty = 1;
+
+    public static int ScoreLimit = 15;
+
+    private static bool scoreLimitReached = false;
+
+    static ScoreManager()
+    {
+        Initialize();
+    }
 
     public static int GetScore(int playerNumber)
     {
@@ -27,8 +33,30 @@ public static class ScoreManager
         SetScore(playerNumber, score);
     }
 
+    public static void Reset()
+    {
+        for (int i = 0; i < PlayerManager.PlayerCount; i++)
+        {
+            SetScore(i + 1, 0);
+        }
+        scoreLimitReached = false;
+    }
+
+    private static void Initialize()
+    {
+        Scores = new int[PlayerManager.PlayerCount];
+        for (int i = 0; i < PlayerManager.PlayerCount; i++)
+        {
+            SetScore(i + 1, 0);
+        }
+    }
+
     private static void SetScore(int playerNumber, int score)
     {
         Scores[playerNumber - 1] = score;
+        if (GetScore(playerNumber) == ScoreLimit)
+        {
+            scoreLimitReached = true;
+        }
     }
 }
