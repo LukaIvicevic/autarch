f2aa5f0e33ff7409258e73d4c6618128643b909e
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-01-30 06:11:05 -0600

    Implement diagonal aim on controller

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -35,34 +35,71 @@ public class Weapon : MonoBehaviour
             wc.Fire();
         }
 
+        Debug.Log(Input.GetAxisRaw("HorizontalAim_P" + controller.playerNumber) + " - " + Input.GetAxisRaw("VerticalAim_P" + controller.playerNumber));
+
         var horizontalAim = Input.GetAxisRaw("HorizontalAim_P" + controller.playerNumber);
-        if (horizontalAim != 0)
+        var verticalAim = Input.GetAxisRaw("VerticalAim_P" + controller.playerNumber);
+
+        // Right
+        if (horizontalAim == 1 && verticalAim == 0)
         {
-            if (horizontalAim == 1)
-            {
-                rotation = Quaternion.Euler(0, 0, 0);
-                transform.rotation = rotation;
-            }
-            else if (horizontalAim == -1)
-            {
-                rotation = Quaternion.Euler(0, 180, 0);
-                transform.rotation = rotation;
-            }
+            rotation = Quaternion.Euler(0, 0, 0);
+            transform.rotation = rotation;
+            return;
+        }
+        
+        // Left
+        if (horizontalAim == -1 && verticalAim == 0)
+        {
+            rotation = Quaternion.Euler(0, 180, 0);
+            transform.rotation = rotation;
+            return;
         }
 
-        var verticalAim = Input.GetAxisRaw("VerticalAim_P" + controller.playerNumber);
-        if (verticalAim != 0)
+        // Up
+        if (verticalAim == 1 && horizontalAim == 0)
         {
-            if (verticalAim == 1)
-            {
-                rotation = Quaternion.Euler(0, 0, 90);
-                transform.rotation = rotation;
-            }
-            else if (verticalAim == -1)
-            {
-                rotation = Quaternion.Euler(0, 0, 270);
-                transform.rotation = rotation;
-            }
+            rotation = Quaternion.Euler(0, 0, 90);
+            transform.rotation = rotation;
+            return;
+        }
+
+        // Down
+        if (verticalAim == -1 && horizontalAim == 0)
+        {
+            rotation = Quaternion.Euler(0, 0, 270);
+            transform.rotation = rotation;
+            return;
+        }
+
+        // Up Right
+        if (verticalAim > 0.4 && verticalAim < 0.8 && horizontalAim > 0.4 && horizontalAim < 0.8)
+        {
+            rotation = Quaternion.Euler(0, 0, 45);
+            transform.rotation = rotation;
+            return;
+        }
+
+        // Up Left
+        if (verticalAim > 0.4 && verticalAim < 0.8 && horizontalAim < -0.4 && horizontalAim > -0.8)
+        {
+            rotation = Quaternion.Euler(0, 0, 135);
+            transform.rotation = rotation;
+            return;
+        }
+
+        // Down Right
+        if (verticalAim < -0.4 && verticalAim > -0.8 && horizontalAim > 0.4 && horizontalAim < 0.8)
+        {
+            rotation = Quaternion.Euler(0, 0, -45);
+            transform.rotation = rotation;
+        }
+
+        // Down Left
+        if (verticalAim < -0.4 && verticalAim > -0.8 && horizontalAim < -0.4 && horizontalAim > -0.8)
+        {
+            rotation = Quaternion.Euler(0, 0, -135);
+            transform.rotation = rotation;
         }
     }
 }
