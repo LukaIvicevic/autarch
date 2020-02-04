e27322e850801ef84d8dae1f645f4f4dac1ab2f3
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-02-04 06:18:22 -0600

    Create sniper rifle

---
+++ Assets/Scripts/SniperBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperBullet : MonoBehaviour
+{
+    public float damage = 20;
+    public float speed = 50f;
+    public Rigidbody2D rb;
+    public CharacterController2D firedBy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        AudioManager.instance.Play("Sniper_Shoot");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var damagedPlayer = collision.GetComponent<CharacterController2D>();
+
+        if (damagedPlayer != null)
+        {
+            damagedPlayer.TakeDamage(damage, firedBy);
+        }
+
+        Destroy(gameObject);
+    }
+}
