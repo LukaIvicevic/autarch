684cc2edf5aea476b595c7febf1035b0e78843f7
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2020-02-05 19:15:05 -0600

    Working on Spike weapon

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Weapons
+﻿public static class Weapons
 {
     public const string Pistol = "Pistol";
 
     public const string RocketLauncher = "RocketLauncher";
 
     public const string SniperRifle = "SniperRifle";
+
+    public const string Spikes = "Spikes";
 }
---
+++ Assets/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Spikes : MonoBehaviour, IWeapon
+{
+    public float damage = 15;
+    public float knockbackModifier = 1;
+    public Rigidbody2D rb;
+    public CharacterController2D firedBy;
+
+    public void Fire()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var damagedPlayer = collision.GetComponent<CharacterController2D>();
+
+        if (damagedPlayer != null)
+        {
+            damagedPlayer.TakeDamage(damage, firedBy);
+            damagedPlayer.Knockback(rb.velocity * knockbackModifier);
+        }
+    }
+}
